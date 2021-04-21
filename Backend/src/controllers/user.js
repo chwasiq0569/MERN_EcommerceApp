2c47cd60e277b11d4a102b1988864dd1fa709de4
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-21 06:43:29 +0500

    Created Seperate Files for controllers models & routes

---
+++ Backend/src/controllers/user.js
@@ -0,0 +1,35 @@
+const User = require("../models/user");
+
+module.exports.signup = (req, res) => {
+    User.findOne({
+        email: req.body.email
+    }).then((user) => {
+        if(user){
+            res.status(400).json({
+                message: "User Already Exists"
+            })
+                }
+        else{
+            const { firstName,lastName, email, password } = req.body;
+            const _user = new User({
+                firstName,
+                lastName, 
+                email, 
+                password, 
+                username: Math.random().toString()});
+            _user.save().then((user) => {
+                return res.status(201).json({ 
+                    message: "User Created Successfully.",
+                    user: user 
+                                            });
+                                    }).catch((err) => {
+                                        return res.status(201).json({
+                                            message: "Something Went Wrong while Saving User!"
+                                        })
+                                    })
+            }
+    }).catch(err => res.status(400).json({
+        message: "Something Went Wrong while Finding or Creating User!!"
+    }))
+    
+}
