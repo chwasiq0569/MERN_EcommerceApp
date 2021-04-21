a8f96d94efdd95465136db3ece0f69aed9feef2c
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-21 06:39:26 +0500

    Created signup route

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -2,8 +2,38 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.get("/signup", (req, res) => {
-    res.send("<h1>HELLO WORLD</h1>")
+router.post("/signup", (req, res) => {
+    User.findOne({
+        email: req.body.email
+    }).then((user) => {
+        if(user){
+            res.status(400).json({
+                message: "User Already Exists"
+            })
+                }
+        else{
+            const { firstName,lastName, email, password } = req.body;
+            const _user = new User({
+                firstName,
+                lastName, 
+                email, 
+                password, 
+                username: Math.random().toString()});
+            _user.save().then((user) => {
+                return res.status(201).json({ 
+                    message: "User Created Successfully.",
+                    user: user 
+                                            });
+                                    }).catch((err) => {
+                                        return res.status(201).json({
+                                            message: "Something Went Wrong while Saving User!"
+                                        })
+                                    })
+            }
+    }).catch(err => res.status(400).json({
+        message: "Something Went Wrong while Finding or Creating User!!"
+    }))
+    
 })
 
 module.exports = router
