0aca9750ab09510a8632e8c8deffd0090b9edf87
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-22 07:36:21 +0500

    Created Basic Layout & Header Component

---
+++ Frontend/admin-app/src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../Header/Header';
+const Layout = (props) => {
+    return ( <>
+                <Header />
+                {props.children}
+            </> );
+}
+ 
+export default Layout;
