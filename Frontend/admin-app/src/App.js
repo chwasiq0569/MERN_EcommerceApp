7a42496acca352b47ce4840591a8398e1f5c0f50
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-25 05:20:54 +0500

    Created basic Home, SignIn, SignUp Component & added Routes to it

--- Frontend/admin-app/src/App.js
+++ Frontend/admin-app/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Layout from './components/Layout/Layout';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './container/Home/Home';
+import SignIn from './container/SignIn/SignIn';
+import SignUp from './container/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Layout>
+      {/* <Layout>
         <h1>CHILDRENS</h1>
-      </Layout>
+      </Layout> */}
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- Frontend/admin-app/src/components/Header/Header.js
+++ Frontend/admin-app/src/components/Header/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const Header = () => {
-    return ( <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-       <Nav className="mr-auto">
-        {/* <Nav.Link href="#features">Features</Nav.Link>
+  return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container>
+        <Navbar.Brand href="#home">Admin Dashboard</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            {/* <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
         <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -16,15 +18,17 @@ const Header = () => {
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
         </NavDropdown> */}
-      </Nav> 
-      <Nav>
-        <Nav.Link href="#deets">SignIn</Nav.Link>
-        {/* <Nav.Link eventKey={2} href="#memes">
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">SignIn</Nav.Link>
+            {/* <Nav.Link eventKey={2} href="#memes">
           SignIn
         </Nav.Link> */}
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar> );
-}
- 
+          </Nav>
+        </Navbar.Collapse>
+    </Container>
+      </Navbar>
+  );
+};
+
 export default Header;
