8fa1cb61094c30959eda3eb59cffaca18eef6c26
Wasiq Abdullah <samsunggrandprimeplus056@gmail.com>
2021-04-29 06:42:23 +0500

    Created add to Cart controller

--- Backend/src/controllers/cart.js
+++ Backend/src/controllers/cart.js
@@ -1,5 +1,35 @@
+const Cart = require("../models/cart")
+
 exports.addItemToCart = (req, res) => {
-    res.status(200).json({
-        msg: "CART"
-    })
+    Cart.findOne({user: req.user._id}).then((productCart) => {  
+        const product = req.body.cartItems.product;
+        if(productCart){
+            const item = productCart.cartItems.find(cartItem => cartItem.product == product);
+                    let condition, update; 
+                    if(item){
+                        condition = { "user": req.user._id, "cartItems.product": product };
+                        update = {
+                                "$set": {
+                                    "cartItems.$": {
+                                        ...req.body.cartItems,
+                                        quantity: item.quantity + req.body.cartItems.quantity
+                                    }
+                                }
+                            }
+                     }
+                     else {
+                         condition = {user: req.user._id};
+                         update = {
+                                "$push": {
+                                    "cartItems": req.body.cartItems 
+                                 }
+                            };
+                     }
+                     Cart.findOneAndUpdate(condition, update).then(cart => res.status(201).json({cart: cart})).catch(error => res.status(400).json({error: error}))
+            }
+            else{
+                const cart = new Cart({user: req.user._id,cartItems: [req.body.cartItems]})
+                cart.save().then(cart => res.status(201).json({ cart: cart })).catch(error => res.status(400).json({error: error}))
+        }
+    }).catch(error => res.status(400).json({error: error}))    
 }
